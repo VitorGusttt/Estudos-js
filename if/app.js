69e9e6725eb0a34cb7a6855fc32eb1e92362b09c
unknown <vittorbeatz@gmail.com>
2023-05-05 08:23:04 -0300

    Curso js. condicionais

---
+++ if/app.js
@@ -0,0 +1,15 @@
+hora = -1;
+if (hora >= 0 && hora <= 11){
+    console.log('Bom dia');
+}
+else if (hora >= 12 && hora <= 17){
+    console.log('boa tarde');
+}
+
+else if (hora >= 18 && hora <= 23){
+    console.log('Boa noite');
+}
+
+else{
+    console.log('Oi, digita uma hora VALIDA ae');
+}
